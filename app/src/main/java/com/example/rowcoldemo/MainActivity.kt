962166848179244a64b45a0d5cf3d354418644b1
tommykorch <tkorchmit@bk.ru>
2025-09-25 00:26:12 +0700

    Different size text

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -38,10 +38,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.height(300.dp)) {
-        TextCell("1", Modifier.align(Alignment.Top))
-        TextCell("2", Modifier.align(Alignment.CenterVertically))
-        TextCell("3", Modifier.align(Alignment.Bottom))
+    Row {
+        Text(
+            text = "Large Text",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Small Text",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 
 
